4679ecd6410da06782554a259d9815eebb07d803
AlinaShestakova <shestakova.ayu@edu.spbstu.ru>
2021-11-01 19:22:00 +0300

    Added 4th homework

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+// Если нужен такого рода импорт, придется поставить rollup scss plugin
+// В остальных случаях все стили подтянет gulp, просто по маске  src/styles/**/*.less и скомпилирует их в единый css файл
+// import './styles/layout.scss';
+
+alert('Javascript Works!');
